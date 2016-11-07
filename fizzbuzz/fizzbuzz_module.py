2dfcf9b3b161eb8bb71fa71c605b43c382609303
Szabó Szilveszter <szaboszilveszter0909@gmail.com>
2016-11-07 18:50:32 +0100

    fizzbuzz kesz

--- fizzbuzz/fizzbuzz_module.py
+++ fizzbuzz/fizzbuzz_module.py
@@ -1,9 +1,30 @@
-def fizzbuzz(number):
-    return
+import sys
+
+def fizzbuzz(x):
+        
+        if x % 15 == 0:
+            return ("FizzBuzz")
+        else:
+            if x % 3 == 0:
+                return ("Fizz")
+            if x % 5 == 0:
+                return ("Buzz")
+        return x
+    
+   
 
 
 def main():
+
+    number = []
+    for i in range(1,101):
+        x=fizzbuzz(i)
+        
+        number.append(x)
+    print(number)
+
     return
+
 
 if __name__ == '__main__':
     main()
